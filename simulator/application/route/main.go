2c8243aef2eb9f89e7009350fe4852b993b83ece
Kaique <kakaluzrda@gmail.com>
2022-06-23 14:07:08 -0300

    route go json

---
+++ simulator/application/route/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/kaiqueluz/imersapfsfc2-simulator/application/route"
+)
+
+func main() {
+	route := route.Route{
+		ID:       "1",
+		ClientID: "1",
+	}
+	route.LoadPositions()
+	stringjson, _ := route.ExportJsonPositions()
+	fmt.Println((stringjson[0]))
+}
