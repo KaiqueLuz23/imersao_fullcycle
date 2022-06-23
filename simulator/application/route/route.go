f94b45030d4bb2528672b185b9f922f796ef5f36
Kaique <kakaluzrda@gmail.com>
2022-06-23 13:54:13 -0300

    route go

--- simulator/application/route/route.go
+++ simulator/application/route/route.go
@@ -59,5 +59,17 @@ func (r *Route) LoadPositions() error {
 
 // pegando json e tranformando em uma lista str
 func (r *Route) ExportJsonPositions() ([]string, error) {
+	var route PartialRoutePOsition
+	var result []string
+	total := len(r.Positions)
 
+	for k, v := range r.Positions {
+		route.ID = r.ID
+		route.ClientID = r.ClientID
+		route.Position = []float64{v.Lat, v.Long}
+		route.Finished = false
+		if total-1 == k {
+			route.Finished = true
+		}
+	}
 }
