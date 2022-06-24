fa2b25d7b39df8de830379d1f18f133cffbef292
Kaique <kakaluzrda@gmail.com>
2022-06-24 15:23:15 -0300

    produce go

--- simulator/main.go
+++ simulator/main.go
@@ -3,15 +3,28 @@ package main
 import (
 	"fmt"
 
-	"github.com/kaiqueluz/imersapfsfc2-simulator/application/route"
+	"github.com/kaiqueluz/imersapfsfc2-simulator/infra/kafka"
 )
 
 func main() {
-	route := route.Route{
-		ID:       "2",
-		ClientID: "2",
+	msgChan := make(chan *ckafka.Message)
+	cosumer := kafka.NewKafkaConsumer(msgChan)
+	go cosumer.Consume()
+
+	for msg := range msgChan {
+		fmt.Println(string(msg.Value))
 	}
-	route.LoadPositions()
-	stringjson, _ := route.ExportJsonPositions()
-	fmt.Println(stringjson[1])
+	//producer := kafka.NewKafkaProducer()
+	//kafka.Publish("Olá", "readtest", producer)
+	// lop infinito
+	//for {
+	//	_ = 1
+	//}
+	//route := route.Route{
+	//	ID:       "2",
+	//	ClientID: "2",
+	//}
+	//route.LoadPositions()
+	//stringjson, _ := route.ExportJsonPositions()
+	//fmt.Println(stringjson[1])
 }
